c22adfe35f97da26f2b081d6d93bce208a2304fa
shoothzj <shoothzj@gmail.com>
2020-09-21 23:22:10 +0800

    提交validate config

---
+++ demo-basic/src/main/java/com/github/shoothzj/demo/basic/ValidationDemo.java
@@ -0,0 +1,21 @@
+package com.github.shoothzj.demo.basic;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+/**
+ * @author hezhangjian
+ * //todo validate
+ */
+@Slf4j
+public class ValidationDemo {
+
+    public static void main(String[] args) {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+    }
+
+}
